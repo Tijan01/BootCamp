e1b9e1834200f68e98257180592efc1ee8778160
Tijan01 <muhammadtijan0@gmail.com>
2022-12-26 16:23:06 +0700

    first commit

---
+++ week1/Qweek1.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO USERS (username, email, password)
+-- 	VALUES
+-- 	('admin', 'admin@gmail.com', 'admin123'),
+-- 	('copen', 'copen@gmail.com', 'copen123'),
+-- 	('rofai', 'rofai@gmail.com', 'rofi123'); 
+	
+-- update users
+-- 	set username = 'choven'
+-- 	where username = 'copen';
+	
+-- select *
+-- 	from users
+
+-- delete From users
+-- 	where username = 'rofai';
+
+select *
+	from users
